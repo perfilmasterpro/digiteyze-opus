e7563c5cff216ff904c69d2f848e80ce3bcc01d6
perfilmasterpro <perfilmasterpro@gmail.com>
2026-09-17 09:19:30 -0300

    Remove rota provisoria duplicada de follow-up

--- src/routes/prospeccao/follow-up.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/prospeccao/follow-up")({
-  component: () => <div className="p-6">Follow-up</div>,
-});
